8a387718b97dd2fc4ee441a062b7302135d278f9
Mohammad12897 <mohammad.mdj.1959@gmail.com>
2026-05-12 11:53:12 +0200

    Remove pasted Edge metadata

--- makro_sim/risk_dashboard/core/presets.py
+++
@@ -1,9 +0,0 @@
-# core/presets.py
-
-import json
-import os
-
-def load_presets():
-    path = os.path.join("data", "slider_presets.json")
-    with open(path, "r") as f:
-        return json.load(f)
--- makro_sim/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="makro_sim",
-    version="0.0.1",
-    description="Makro Dashboard",
-    packages=find_packages(),
-    python_requires=">=3.9",
-)
